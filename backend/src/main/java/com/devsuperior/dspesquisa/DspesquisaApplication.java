694bd975742437099c36a24a5ac65e765b0f93f9
Gustavo Malheiro <gus.malheiro@gmail.com>
2020-09-15 22:41:16 -0300

    Entities, Repositories

--- backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
@@ -3,6 +3,8 @@ package com.devsuperior.dspesquisa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//anotation que represneta nossa aplicação spring boot
+
 @SpringBootApplication
 public class DspesquisaApplication {
 
