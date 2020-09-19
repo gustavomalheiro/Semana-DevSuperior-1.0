c5a71783fc7545ff4e19e5209bbf8debcffd83dc
Gustavo Malheiro <gus.malheiro@gmail.com>
2020-09-18 23:48:07 -0300

    Finalizando app React Native

---
+++ front-mobile/src/pages/CreateRecords/types.ts
@@ -0,0 +1,11 @@
+import { Platform } from "react-native";
+
+export type GamePlatform = 'XBOX' | 'PC' | 'PLAYSTATION';
+
+export type Game = {
+    id: number;
+    title: string;
+    platform: GamePlatform;
+    label: string;
+    value: number;
+}
